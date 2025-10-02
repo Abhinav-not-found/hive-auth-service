98e1223e9e3812fd59dca1fb5edaa18e6672fd80
abhinav <abhinavisgreat18@gmail.com>
2025-10-02 22:40:40 +0530

    updated controller : getUserById select(-password)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -144,7 +144,7 @@ export const uploadProfilePic = async (req, res, next) => {
 export const getUserInfoById = async (req, res, next) => {
   try {
     const { userId } = req.params;
-    const user = await User.findById(userId).select("name bio username profileImage");
+    const user = await User.findById(userId).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
     res.status(200).json(user);
   } catch (error) {
