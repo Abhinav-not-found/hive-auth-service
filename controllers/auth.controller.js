4b86ac771629c8204d990985cf163ccd0bcee336
abhinav <abhinavisgreat18@gmail.com>
2025-10-02 22:25:45 +0530

    minor update - login: id to _id

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -69,7 +69,7 @@ export const login = async (req, res, next) => {
 
     res.status(200).json({
       message: "Login successful",
-      user: { id: user._id, name: user.name, email: user.email, role: user.role },
+      user: { _id: user._id, name: user.name, email: user.email, role: user.role },
     });
 
 
